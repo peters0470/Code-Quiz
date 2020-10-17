050621850d9046a046595ccf2e58751bc50bf6a7
unknown <peters0470@gmail.com>
2020-10-16 19:45:04 -0500

    started to add variable into javascript and changed id in html

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var startButton = document.querySelector("#start-quiz");
+var quizContainer = document.querySelector("quiz-box");
+var quizButton = document.querySelector(".btn");
+var timer = document.querySelector("#timer");
+var quizHeader = document.querySelector("#quiz-header");
+var quizText = document.querySelector("#quiz-text");
+var highScore = doument.querySelector("#high-score");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+startButton.addEventListener("click",);
+quizContainer.addEventListener("click",);
