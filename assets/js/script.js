7c2a805518c800de39a545ab4f27823697546800
unknown <peters0470@gmail.com>
2020-10-16 20:35:33 -0500

    added questions into javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,21 @@ var quizHeader = document.querySelector("#quiz-header");
 var quizText = document.querySelector("#quiz-text");
 var highScore = doument.querySelector("#high-score");
 
-
+var questions = [
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: {
+            0: "numbers and strings",
+            1: "other arrays",
+            2: "booleans",
+            3: "all of the above"
+        },
+        alerts: "all of the above"
+    },
+    {
+        
+    }
+]
 
 
 
