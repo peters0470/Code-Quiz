6c85254c812952963235252ae3f3daba3b4d478b
unknown <peters0470@gmail.com>
2020-10-17 10:38:33 -0500

    made function to set scores in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,14 @@ var buttonEl;
 var timeStart = 75;
 var questionsAnswer;
 var questionsScore = 0;
-var leaderBoard= []
-var x = 0
+var leaderBoard = [];
+var x = 0;
 
 var beginQuiz = function() {
     var startTime = setInterval(function(){
         if (timeStart <= -1 || x === 5) {
             clearInterval(startTime);
+            endGame();
         } else {
             timer.textContent = timeStart;
         }
@@ -27,16 +28,16 @@ var beginQuiz = function() {
 
 var firstQuestion = function() {
     if (x === 5) {
-        return startQuiz;
+        return beginQuiz;
     }
     quizHeader.textContent = questions[x].title;
     quizText.textContent = '';
     startButton.remove();
     var listChoice = questions[x].choices;
     questionsAnswer = questions[x].alerts;
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 4; i++) {
         listEl = document.createElement("li");
-        listEl.className = "btn-cont";
+        listEl.className = "btn-content";
         listEl.setAttribute("id", [i]);
         quizContainer.appendChild(listEl);
         buttonEl = document.createElement("button");
@@ -58,18 +59,22 @@ var buttonHandler = function(event) {
             timeStart -= 10;
             console.log(timeStart);
         };
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 4; i++) {
             var list = document.getElementById([i]);
             list.remove();
         }
         x++
         firstQuestion();
+    }
+    if (targetEl.matches(".btn-submit")) {
+        submitInitials();
     }   
+
 }
 
 var endGame = function() {
     score = Math.max(0, timeStart + questionsScore);
-    quizHeaer.textContent = "Game Over";
+    quizHeader.textContent = "Game Over";
     quizText.textContent = "Your Score:" + score;
     var initials = document.createElement("input");
     initials.setAttribute("type", "text");
@@ -77,7 +82,25 @@ var endGame = function() {
     initials.setAttribute("placeholder", "Your initials");
     quizContainer.appendChild(initials);
     
+    var submit = document.createElement("button");
+    submit.className = "btn-submit";
+    submit.setAttribute("id", "save-task");
+    submit.setAttribute("type", "submit");
+    submit.textContent = "Submit";
+    quizContainer.appendChild(submit);
 }
+
+var submitInitials = function() {
+    var submitInitials = document.querySelector(".text-input").value;
+    var submitScore = timeStart + score;
+    var playerScore = {
+        name: submitInitials,
+        total: submitScore
+    }
+    leaderBoard.push(playerScore);
+    localStorage.setItem("highScores", JSON.stringify(leaderBoard));
+}
+
 
 var questions = [
     {
